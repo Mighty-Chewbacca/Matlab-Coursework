714448985f4b5ac0bc855d4274df6af476eed6dd
Mighty-Chewbacca <alexmalcolm93@live.co.uk>
2014-11-10 22:51:10 +0000

    misc

--- MainScript.m
+++ MainScript.m
@@ -2,3 +2,4 @@
 % individual strings, we can then save these into an array or just as
 % seperate variables from there.
 % these variables can then easily be used inside scripts like this!
+% http://www.mathworks.co.uk/help/matlab/creating_guis/add-code-for-components-in-callbacks.html#f10-1001464
