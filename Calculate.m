4df8b78c995e2107ed82a8046e6df4f3d579bec1
Mighty-Chewbacca <alexmalcolm93@live.co.uk>
2014-12-01 12:51:25 +0000

    fucking updates are done

--- Calculate.m
+++ Calculate.m
@@ -1,156 +1,109 @@
-% for gui stuff all we need is "input = get(hObject,'String');" to grab the
-% individual strings, we can then save these into an array or just as
-% seperate variables from there.
-% these variables can then easily be used inside scripts like this!
-% http://www.mathworks.co.uk/help/matlab/creating_guis/add-code-for-components-in-callbacks.html#f10-1001464
 
-% list of variables gotten from UI (for reference purposes) in the order
-% they are created
+%initially set up variables for the program to run :) -AM
 
-% initialSpeed
-% angleOfMotion
-% coefficientOfRestitution
-% wallDistance
-% wallHeight
-% ballStartingHeight
-% ballRadius
-% ballMass
-% airResistance
-
-%handles to the output text data
-%XPWReturn
-%YPWReturn
-%TWReturn
-%XPGReturn
-%YPGReturn
-%TGReturn
-%XCPReturn
-%YCPReturn
-%CTReturn
-
-hasHitWall = false;
-hasHitGround = false;
+isProgramRunning = 1;
+isCalculating = 1;
+hasHitWall = 0;
+hasHitGround = 0;
 
 initialX = ballRadius;
 initialY = ballRadius + ballStartingHeight;
-
 airRes = airResistance;
 timeStep = 0.01;
 mg = ballMass * 9.81;
 time = 0;
-
 initialMS = initialSpeed;
 initialAngle = angleOfMotion;
-
 %need to convert to radians for matlabby stuff
-angleInRadians = degtorad(initialAngle);
-
+%angleInRadians = degtorad(initialAngle);
+angleInRadians = initialAngle * (pi / 180);
 % calculate the initial speeds in relation to angles as well
 initialSpeedX = initialMS * cos(angleInRadians);
 initialSpeedY = initialMS * sin(angleInRadians);
-
 previousSpeedX = initialSpeedX;
 previousSpeedY = initialSpeedY;
-
 currentX = initialX;
 currentY = initialY;
-
 xPos = initialX;
 yPos = initialY;
-
 %tell it to draw to axes on ui
 axes(handles.ballPlot);
-
-% main statement for the ball moving in the first place, need to add bool
-% and check if wall has been hit yet
-while(xPos < wallDistance - ballRadius)
-    
-    % calculate the x positions using the current speed
-    xPos = currentX + (timeStep * previousSpeedX);
-    yPos = currentY + (timeStep * previousSpeedY);
-
-    nextSpeedX = previousSpeedX + timeStep * (-airRes * previousSpeedX);
-    nextSpeedY = previousSpeedY + timeStep * (-airRes * previousSpeedY -mg);
-
-    currentX = xPos;
-    currentY = yPos;
-
-    previousSpeedX = nextSpeedX;
-    previousSpeedY = nextSpeedY;
-
-    time = time + timeStep;
-    disp(time);
-
-    %plot (xPos, yPos);
-    coordMatrix = [xPos, yPos];
-    viscircles(coordMatrix,ballRadius);
-    line([-1 wallDistance], [0 0], 'LineWidth', 4, 'color', 'r'); % floor line
-    line([wallDistance wallDistance], [-1 wallHeight], 'LineWidth', 4, 'color', 'g'); % wall ball is hitting
-
-    pause(0.005);
-
-    if (xPos < wallDistance - ballRadius)
-        cla(handles.ballPlot);
-    end
-    
-    if (yPos <= 0)
-        disp ('hit ground');
-        break
-    end
-
-end
-
-if (xPos >= wallDistance - ballRadius)
-    disp('the wall has been hit by the ball at');
-    disp(time);
-    disp('seconds');
-    disp('Now do some calculations to make it bounce!');
-    
-    hasHitWall = true;
-
-    disp(xPos); % will display the final location on the x axis of the ball before it hit the wall
-    disp(yPos); % will display the final location on the y axis of the ball before it hit the wall
-
-    % apply coefficient to get new velocities
-    bounceBackSpeedX = -coefficientOfRestitution*(previousSpeedX);
-    bounceBackSpeedY = -coefficientOfRestitution*(previousSpeedY);
-end
-
-while (yPos > ballRadius && hasHitWall == true)
-    
-    cla(handles.ballPlot);
-    
-    % calculate the x positions using the current speed
-    xPos = currentX + (timeStep * bounceBackSpeedX);
-    yPos = currentY + (timeStep * bounceBackSpeedY);
-
-    nextSpeedX = bounceBackSpeedX + timeStep * (-airRes * bounceBackSpeedX);
-    nextSpeedY = bounceBackSpeedY + timeStep * (-airRes * bounceBackSpeedY -mg);
-    
-    currentX = xPos;
-    currentY = yPos;
-
-    bounceBackSpeedX = nextSpeedX;
-    bounceBackSpeedY = nextSpeedY;
-
-    time = time + timeStep;
-    disp(time);
-    
-    %plot (xPos, yPos);
-    coordMatrix = [xPos, yPos];
-    viscircles(coordMatrix,ballRadius);
-    line([-1 wallDistance], [0 0], 'LineWidth', 4, 'color', 'r'); % floor line
-    line([wallDistance wallDistance], [-1 wallHeight], 'LineWidth', 4, 'color', 'g'); % wall ball is hitting
-
-    pause(0.01);
-    
-    % if the ball falls below the floor
-    if (yPos <= 0)
-        disp('below ground');
-        break
-    end
-        
-end
-    
 grid on;
+
+while(isProgramRunning == 1) % main simulation loop, will exit when simulation is over -AM
+   
+    if(isCalculating == 1)
+        % calculate the x positions using the current speed
+        xPos = currentX + (timeStep * previousSpeedX);
+        yPos = currentY + (timeStep * previousSpeedY);
+
+        nextSpeedX = previousSpeedX + timeStep * (-airRes * previousSpeedX);
+        nextSpeedY = previousSpeedY + timeStep * (-airRes * previousSpeedY -mg);
+
+        currentX = xPos;
+        currentY = yPos;
+
+        previousSpeedX = nextSpeedX;
+        previousSpeedY = nextSpeedY;
+
+        time = time + timeStep;
+
+        % code to update the current value of the static text that will display
+        % the outputs. -AM
+        set(handles.CTReturn, 'String', num2str(time));
+        set(handles.CXPReturn, 'String', num2str(currentX));
+        set(handles.CYPReturn, 'String', num2str(currentY));
+        
+
+        %plot (xPos, yPos);
+        coordMatrix = [xPos, yPos];
+        viscircles(coordMatrix,ballRadius);
+        line([-1 wallDistance], [0 0], 'LineWidth', 4, 'color', 'r'); % floor line
+        line([wallDistance wallDistance], [-1 wallHeight], 'LineWidth', 4, 'color', 'g'); % wall ball is hitting
+
+        pause(0.001);
+        
+        if(currentX >= wallDistance - ballRadius && currentY <= wallHeight - ballRadius && hasHitWall == 0)
+            % we have hit the wall
+            % now need to run coefficient calculations
+            
+            disp('the wall has been hit by the ball at');
+            disp(time);
+            disp('seconds');
+            disp('Now do some calculations to make it bounce!');
+            
+            % code to update the wall values  of the static text that will display
+            % the outputs. -AM
+            set(handles.TWReturn, 'String', num2str(time));
+            set(handles.XPWReturn, 'String', num2str(currentX));
+            set(handles.YPWReturn, 'String', num2str(currentY));
+            
+            % apply coefficient to get new velocities
+            previousSpeedX = -coefficientOfRestitution*(previousSpeedX);
+            previousSpeedY = -coefficientOfRestitution*(previousSpeedY);
+            
+            hasHitWall = 1;
+        end
+        
+        if(currentY <= ballRadius)
+            % ball has now hit the ground
+            disp('the ball has hit the ground');
+            disp(time);
+            disp('seconds');
+            
+            % code to update the wall values  of the static text that will display
+            % the outputs. -AM
+            set(handles.TWReturn, 'String', num2str(time));
+            set(handles.XPWReturn, 'String', num2str(currentX));
+            set(handles.YPWReturn, 'String', num2str(currentY));
+            
+            isCalculating = 0; % should stop the calculations from continuing when hitting the ground
+            
+        end
+        
+        cla(handles.ballPlot);
+        
+    end
+    
+end
 
---
+++ tempMFile.m
@@ -0,0 +1,166 @@
+% for gui stuff all we need is "input = get(hObject,'String');" to grab the
+% individual strings, we can then save these into an array or just as
+% seperate variables from there.
+% these variables can then easily be used inside scripts like this!
+% http://www.mathworks.co.uk/help/matlab/creating_guis/add-code-for-components-in-callbacks.html#f10-1001464
+
+% list of variables gotten from UI (for reference purposes) in the order
+% they are created
+
+% initialSpeed
+% angleOfMotion
+% coefficientOfRestitution
+% wallDistance
+% wallHeight
+% ballStartingHeight
+% ballRadius
+% ballMass
+% airResistance
+
+%handles to the output text data
+%XPWReturn
+%YPWReturn
+%TWReturn
+%XPGReturn
+%YPGReturn
+%TGReturn
+%XCPReturn
+%YCPReturn
+%CTReturn
+%CYVReturn
+%CXVReturn
+
+hasHitWall = false;
+hasHitGround = false;
+
+initialX = ballRadius;
+initialY = ballRadius + ballStartingHeight;
+
+airRes = airResistance;
+timeStep = 0.01;
+mg = ballMass * 9.81;
+time = 0;
+
+initialMS = initialSpeed;
+initialAngle = angleOfMotion;
+
+%need to convert to radians for matlabby stuff
+%angleInRadians = degtorad(initialAngle); %this didnt work for some reason
+
+angleInRadians = initialAngle * (pi / 180);
+
+% calculate the initial speeds in relation to angles as well
+initialSpeedX = initialMS * cos(angleInRadians);
+initialSpeedY = initialMS * sin(angleInRadians);
+
+previousSpeedX = initialSpeedX;
+previousSpeedY = initialSpeedY;
+
+currentX = initialX;
+currentY = initialY;
+
+xPos = initialX;
+yPos = initialY;
+
+%tell it to draw to axes on ui
+axes(handles.ballPlot);
+
+% main statement for the ball moving in the first place, need to add bool
+% and check if wall has been hit yet
+while(xPos < wallDistance - ballRadius)
+    
+    % calculate the x positions using the current speed
+    xPos = currentX + (timeStep * previousSpeedX);
+    yPos = currentY + (timeStep * previousSpeedY);
+
+    nextSpeedX = previousSpeedX + timeStep * (-airRes * previousSpeedX);
+    nextSpeedY = previousSpeedY + timeStep * (-airRes * previousSpeedY -mg);
+
+    currentX = xPos;
+    currentY = yPos;
+
+    previousSpeedX = nextSpeedX;
+    previousSpeedY = nextSpeedY;
+
+    time = time + timeStep;
+    disp(time);
+    
+    % code to update the current value of the static text that will display
+    % the outputs. -AM
+    set(handles.CTReturn, 'String', num2str(time));
+    set(handles.CPXReturn, 'String', num2str(currentX));
+    set(handles.CPYReturn, 'String', num2str(currentY));
+
+    %plot (xPos, yPos);
+    coordMatrix = [xPos, yPos];
+    viscircles(coordMatrix,ballRadius);
+    line([-1 wallDistance], [0 0], 'LineWidth', 4, 'color', 'r'); % floor line
+    line([wallDistance wallDistance], [-1 wallHeight], 'LineWidth', 4, 'color', 'g'); % wall ball is hitting
+
+    pause(0.001);
+
+    if (xPos < wallDistance - ballRadius)
+        cla(handles.ballPlot);
+    end
+    
+    if (yPos <= 0)
+        disp ('hit ground');
+        break
+    end
+
+end
+
+if (xPos >= wallDistance - ballRadius)
+    disp('the wall has been hit by the ball at');
+    disp(time);
+    disp('seconds');
+    disp('Now do some calculations to make it bounce!');
+    
+    hasHitWall = true;
+
+    disp(xPos); % will display the final location on the x axis of the ball before it hit the wall
+    disp(yPos); % will display the final location on the y axis of the ball before it hit the wall
+
+    % apply coefficient to get new velocities
+    bounceBackSpeedX = -coefficientOfRestitution*(previousSpeedX);
+    bounceBackSpeedY = -coefficientOfRestitution*(previousSpeedY);
+end
+
+while (yPos > ballRadius && hasHitWall == true)
+    
+    cla(handles.ballPlot);
+    
+    % calculate the x positions using the current speed
+    xPos = currentX + (timeStep * bounceBackSpeedX);
+    yPos = currentY + (timeStep * bounceBackSpeedY);
+
+    nextSpeedX = bounceBackSpeedX + timeStep * (-airRes * bounceBackSpeedX);
+    nextSpeedY = bounceBackSpeedY + timeStep * (-airRes * bounceBackSpeedY -mg);
+    
+    currentX = xPos;
+    currentY = yPos;
+
+    bounceBackSpeedX = nextSpeedX;
+    bounceBackSpeedY = nextSpeedY;
+
+    time = time + timeStep;
+    disp(time);
+    
+    %plot (xPos, yPos);
+    coordMatrix = [xPos, yPos];
+    viscircles(coordMatrix,ballRadius);
+    line([-1 wallDistance], [0 0], 'LineWidth', 4, 'color', 'r'); % floor line
+    line([wallDistance wallDistance], [-1 wallHeight], 'LineWidth', 4, 'color', 'g'); % wall ball is hitting
+
+    pause(0.01);
+    
+    % if the ball falls below the floor
+    if (yPos <= 0)
+        disp('below ground');
+        break
+    end
+        
+end
+    
+grid on;
+
